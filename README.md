dcc07a961ed7215a439d4787a9884af69e1eca15
Rogolist <>
2025-06-08 19:33:14 +0200

    some changes

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ To enable or disable Hold the Note
 !bard_hold_on
 
 !bard_hold_off
+
+To change mode
+
+!bard_column
+
+!bard_row
--- main.lua
+++ main.lua
@@ -2,26 +2,48 @@ local api = require("api")
 
 local bard_helper = {
   name = "Bard Helper",
-  version = "0.1",
-  author = "Waifu",
+  version = "0.2",
+  author = "Waifu & Psejik",
   desc = "Shows songs time remaining"
 }
 
+-- controls via chat:
+-- "!bard_hold_on"
+-- "!bard_hold_off"
+-- "!bard_column"
+-- "!bard_row"
 
-local settings
 
+--[[
+	Сделано:
+	- возможность двигать Canvas - есть (темный квадратик слева от иконок песен)
+	- отображение в столбик или рядок
+
+	Что еще нужно сделать:
+	- автоматическое отображение прерывания песен при получении одного из списка дебафов (сон, стан, фир, пузырь...)
+
+]]
+
+local settings = {}
+local Canvas
+local delta_coord = 50
+
+--[[
 local function SaveSettings(hold)
   settings.HoldTheNote = hold
   api.Log:Info("Hold the Note set to " .. tostring(hold))
   api.SaveSettings()
 end
+]]
 
-
+-- библиотека песен
 local songsTimeRemains = {
   {
     title="Quickstep",
+	songDuration = 15,
+	--if settings.HoldTheNote then songDuration = 30 else songDuration = 15 end
     buffId=803,
-    y_coord=0,
+    --delta_coord=0,
     timeUsed=0,
     buffLostTime=0,
     icon=nil,
@@ -29,8 +51,9 @@ local songsTimeRemains = {
   },
   {
     title="Bloody Chantey",
+	songDuration = 15,
     buffId=850,
-    y_coord=50,
+    --delta_coord=50,
     timeUsed=0,
     buffLostTime=0,
     icon=nil,
@@ -38,8 +61,9 @@ local songsTimeRemains = {
   },
   {
     title="Bulwark Ballad",
+	songDuration = 15,
     buffId=1000,
-    y_coord=100,
+    --delta_coord=100,
     timeUsed=0,
     buffLostTime=0,
     icon=nil,
@@ -47,36 +71,50 @@ local songsTimeRemains = {
   },
   {
     title="Ode to Recovery",
-    buffId=834,
-    y_coord=150,
+	songDuration = 15,
+    buffId=834,	-- Ode to Recovery (Rank 2)
+    --delta_coord=150,
     timeUsed=0,
     buffLostTime=0,
     icon=nil,
     label=nil
-  }
+  },
+  {
+    title="Grief's Cadence",
+	songDuration = 15,
+    buffId=6830,	-- Ode to Recovery (Rank 2)
+    --delta_coord=150,
+    timeUsed=0,
+    buffLostTime=0,
+    icon=nil,
+    label=nil
+  },
+  
+
+  {
+    title="Alarm Call",
+	songDuration = 15, --9 if Sleep
+    buffId=2362,
+    --delta_coord=150,
+    timeUsed=0,
+    buffLostTime=0,
+    icon=nil,
+    label=nil
+  },
+  
+  {
+    title="Polymorphic Resonance",
+	songDuration = 25,
+    buffId=20473, --18506, -- or 20473
+    --delta_coord=150,
+    timeUsed=0,
+    buffLostTime=0,
+    icon=nil,
+    label=nil
+  },
 }
 
-
-Canvas = api.Interface:CreateEmptyWindow("BuffAlerterCanvas")
-Canvas:Show(true)
-Canvas:AddAnchor("CENTER", "UIParent", -100, -300)
-
-
-local function createSongUI(song)
-  song.icon = CreateItemIconButton("SongIcon_" .. song.buffId, Canvas)
-  song.icon:Show(false)
-  song.icon:AddAnchor("TOPLEFT", Canvas, "TOPLEFT", -20 + song.y_coord, -20)
-
-  F_SLOT.ApplySlotSkin(song.icon, song.icon.back, SLOT_STYLE.BUFF)
-
-  song.label = Canvas:CreateChildWidget("label", "label_" .. song.buffId, 0, true)
-  song.label:AddAnchor("TOPLEFT", Canvas, "TOPLEFT", song.y_coord, 0)
-  song.label.style:SetFontSize(30)
-  song.label.style:SetShadow(true)
-  song.label:Show(false)
-end
-
-
+-- вызывается с уже измененной длительностью
 local function UpdateSongIcon(song, timeRemains)
   local pathToImage = api.Ability:GetBuffTooltip(song.buffId).path
   F_SLOT.SetIconBackGround(song.icon, pathToImage)
@@ -85,11 +123,13 @@ local function UpdateSongIcon(song, timeRemains)
 end
 
 
+-- какая-то непонятная пока проверка
 local function checkPlayerHasBuff(buffName)
     local buffCount = api.Unit:UnitBuffCount("player")
 
     if buffCount > 0 then
         for i = 1, buffCount do
+			-- перебираем все бафы на игроке
             local buff = api.Unit:UnitBuff("player", i)
 
             if buff and buff.buff_id then
@@ -110,101 +150,283 @@ local function parseTime(time)
   return tonumber(time:sub(8, 11))
 end
 
+-- отдельная функция чтобы переключать длительность песен от значения в настройках
 local function getSongDuration()
   if settings.HoldTheNote then
-    return 30
+    return 15
   end
 
-  return 15
+  return 0
 end
 
 
+-- анализ сообщения боевого чата
 local function updateSongTimeUsed(casterName, skillName)
-  local playerName = api.Unit:GetUnitNameById(api.Unit:GetUnitId("player"))
 
-  if casterName ~= playerName then
-    return
-  end
+	local playerName = api.Unit:GetUnitNameById(api.Unit:GetUnitId("player"))
 
-  local currentTime = parseTime(api.Time.GetLocalTime())
+	if casterName ~= playerName then
+		return
+	end
 
-  for i = 1, #songsTimeRemains do
-    local song = songsTimeRemains[i]
+	local currentTime = parseTime(api.Time.GetLocalTime())
 
-    if "[Perform] " .. song.title == skillName then
-      song.timeUsed = currentTime
-      song.buffLostTime = 0
-      song.icon:Show(true)
-      song.label:Show(true)
+	for i = 1, #songsTimeRemains do
+		local song = songsTimeRemains[i]
 
-      UpdateSongIcon(song, getSongDuration())
-    end
-  end
+		if "[Perform] " .. song.title == skillName then
+			song.timeUsed = currentTime
+			song.buffLostTime = 0
+			song.icon:Show(true)
+			song.label:Show(true)
+			
+			local duration = song.songDuration + getSongDuration()
+
+			--if settings.HoldTheNote then duration = song.songDuration + 15 else duration = song.songDuration end
+
+			--UpdateSongIcon(song, timeRemains)
+			UpdateSongIcon(song, duration)
+			
+		elseif song.title == skillName then
+			song.timeUsed = currentTime
+			song.buffLostTime = 0
+			song.icon:Show(true)
+			song.label:Show(true)
+
+			UpdateSongIcon(song, song.songDuration)
+		
+		end
+	end
+
+	-- добавить сюда как-то отметку про надевание акваланга
+	-- в логе это Acquired: [Mistral Underwarer Breathing Device]
 end
 
 
 local function OnUpdate()
-  local currentTime = parseTime(api.Time.GetLocalTime())
+	local currentTime = parseTime(api.Time.GetLocalTime())
 
-  for i = 1, #songsTimeRemains do
-    local song = songsTimeRemains[i]
+	for i = 1, #songsTimeRemains do
+		local song = songsTimeRemains[i]
 
-    if song.timeUsed > 0 then
-      local timeRemains = song.timeUsed + getSongDuration() - currentTime
+		if song.timeUsed > 0 then
+			--local timeRemains = song.timeUsed + getSongDuration() - currentTime
+			local timeRemains
 
-      if timeRemains > 0 then
-        if checkPlayerHasBuff(song.title) then
-          song.buffLostTime = 0
-          UpdateSongIcon(song, timeRemains)
-        else
-          if song.buffLostTime == 0 then
-            song.buffLostTime = currentTime
-          elseif currentTime - song.buffLostTime > 1 then
-            song.icon:Show(false)
-            song.label:Show(false)
-            song.timeUsed = 0
-            song.buffLostTime = 0
-          end
-        end
-      else
-        song.icon:Show(false)
-        song.label:Show(false)
-      end
+			if song.buffId ==2362 or song.buffId ==20473 then
+				timeRemains = song.timeUsed + song.songDuration - currentTime
+			else
+				timeRemains = song.timeUsed + song.songDuration + getSongDuration() - currentTime
+			end
+
+			if timeRemains > 0 then
+			-- странное обнуление длительности если
+				if checkPlayerHasBuff(song.title) then
+					song.buffLostTime = 0
+					UpdateSongIcon(song, timeRemains)
+				else
+					if song.buffLostTime == 0 then
+						song.buffLostTime = currentTime
+					elseif currentTime - song.buffLostTime > 1 then
+						song.icon:Show(false)
+						song.label:Show(false)
+						song.timeUsed = 0
+						song.buffLostTime = 0
+					end
+				end
+			else
+				song.icon:Show(false)
+				song.label:Show(false)
+			end
+		end
+	end
+end
+
+--[[
+local function getColumnMode()
+	if settings.column then
+		return true
+	end
+	
+	return false
+end
+]]
+
+-- отрисовка иконок песен
+local function createSongUI(i, settings, Canvas)
+	local song = songsTimeRemains[i]
+
+	song.icon = CreateItemIconButton("SongIcon_" .. song.buffId, Canvas)
+	song.icon:Show(false)
+	--song.icon:AddAnchor("TOPLEFT", Canvas, "TOPLEFT", -20 + song.y_coord, -20)
+	
+	--local column = settings.column -- or true
+	--local column = getColumnMode()
+	
+	if settings.column then
+		--debug
+		--api.Log:Info("[BH debug: Column]")
+		
+		song.icon:AddAnchor("TOPLEFT", Canvas, "TOPLEFT", 20 , 0 + (i-1) * delta_coord)
+	else
+		--debug
+		--api.Log:Info("[BH debug: Row]")
+	
+		song.icon:AddAnchor("TOPLEFT", Canvas, "TOPLEFT", 20 + (i-1) * delta_coord, 0)
+	end
+
+	F_SLOT.ApplySlotSkin(song.icon, song.icon.back, SLOT_STYLE.BUFF)
+
+	song.label = Canvas:CreateChildWidget("label", "label_" .. song.buffId, 0, true)
+	--song.label:AddAnchor("TOPLEFT", Canvas, "TOPLEFT", song.y_coord, 0)
+
+	-- отрисовка таймера на песне
+	if settings.column then
+		song.label:AddAnchor("TOPLEFT", Canvas, "TOPLEFT", 40, 20 + (i-1) * delta_coord)
+	else
+		song.label:AddAnchor("TOPLEFT", Canvas, "TOPLEFT", 40 + (i-1) * delta_coord, 20)
+	end
+	
+	song.label.style:SetFontSize(30)
+	song.label.style:SetShadow(true)
+	song.label:Show(false)
+end
+
+function CreateMainDisplay(settings)
+
+	local canvas_x = settings.x or 1300
+	local canvas_y = settings.y or 200
+	
+	Canvas = api.Interface:CreateEmptyWindow("BuffAlerterCanvas", "UIParent")
+
+	Canvas:AddAnchor("TOPLEFT", "UIParent", canvas_x * api.Interface:GetUIScale(), canvas_y * api.Interface:GetUIScale())
+
+	--[[
+	if canvas_x ~= 100 and canvas_y ~= 0 then
+		Canvas:AddAnchor("TOPLEFT", "UIParent", canvas_x * api.Interface:GetUIScale(), canvas_y * api.Interface:GetUIScale())
+	else
+		Canvas:AddAnchor("LEFT", "UIParent", canvas_x * api.Interface:GetUIScale(), canvas_y * api.Interface:GetUIScale())
+	end
+	]]
+	
+	
+	Canvas.bg = Canvas:CreateNinePartDrawable(TEXTURE_PATH.HUD, "background")
+	Canvas.bg:SetTextureInfo("bg_quest")
+	Canvas.bg:SetColor(0, 0, 0, 0.5)
+	Canvas.bg:AddAnchor("TOPLEFT", Canvas, 0, 0)
+	--Canvas.bg:AddAnchor("BOTTOMRIGHT", Canvas, -10, 10)
+	
+	-- размеры темного пятнышка
+	Canvas:SetExtent(25, 25)
+	Canvas:Show(true)
+	
+    -- drag events for main window
+    function Canvas:OnDragStart(arg)
+        Canvas:StartMoving()
+        api.Cursor:ClearCursor()
+        api.Cursor:SetCursorImage(CURSOR_PATH.MOVE, 0, 0)
     end
-  end
+    function Canvas:OnDragStop()
+        Canvas:StopMovingOrSizing()
+        local x, y = Canvas:GetEffectiveOffset()
+        api.Cursor:ClearCursor()
+
+	    settings.x = x
+		settings.y = y
+		api.SaveSettings()
+    end
+
+
+    Canvas:SetHandler("OnDragStart", Canvas.OnDragStart)
+    Canvas:SetHandler("OnDragStop", Canvas.OnDragStop)
+	
+	
+    if Canvas.RegisterForDrag ~= nil then
+        Canvas:RegisterForDrag("LeftButton")
+    end
+    if Canvas.EnableDrag ~= nil then Canvas:EnableDrag(true) end
+	
+	
+	Canvas:EnableDrag(true)
+	
+	-- изменение через чат: наличие пассивного таланта "Hold the Note"
+	function Canvas:OnEvent(event, ...)
+		if event == "COMBAT_MSG" then
+			updateSongTimeUsed(arg[3], arg[6])
+		end
+
+		if event == "CHAT_MESSAGE" then
+			if arg[5] == "!bard_hold_on" then
+				--SaveSettings(true)
+				settings.HoldTheNote = true
+				api.Log:Info("[BardHelper] Hold the Note set to " .. tostring(settings.HoldTheNote))
+				api.SaveSettings()
+			  
+			elseif arg[5] == "!bard_hold_off" then
+				--SaveSettings(false)
+				settings.HoldTheNote = false
+				api.Log:Info("[BardHelper] Hold the Note set to " .. tostring(settings.HoldTheNote))
+				api.SaveSettings()
+				
+			-- переключение режима отображения (столбик или рядок)
+			elseif arg[5] == "!bard_column" then
+				settings.column = true
+				api.Log:Info("[BardHelper] column mode " .. tostring(settings.column))
+				api.SaveSettings()
+
+				-- перерисовка позиций для иконок
+				for i = 1, #songsTimeRemains do
+					-- тут нужно почистить от старых иконок иначе они остаются висеть навечно
+					songsTimeRemains[i].icon:Show(false)
+					songsTimeRemains[i].label:Show(false)
+				
+					--createSongUI(songsTimeRemains[i], settings, Canvas, i)
+					createSongUI(i, settings, Canvas)
+				end
+			elseif arg[5] == "!bard_row" then
+				settings.column = false
+				api.Log:Info("[BardHelper] column mode " .. tostring(settings.column))
+				api.SaveSettings()
+				
+				for i = 1, #songsTimeRemains do
+					songsTimeRemains[i].icon:Show(false)
+					songsTimeRemains[i].label:Show(false)
+					createSongUI(i, settings, Canvas)
+				end
+			end
+			
+		end
+	end
+	
+	
+	for i = 1, #songsTimeRemains do
+		--createSongUI(songsTimeRemains[i], settings, Canvas)
+		createSongUI(i, settings, Canvas)
+	end
+	
+
 end
 
 
-function Canvas:OnEvent(event, ...)
-  if event == "COMBAT_MSG" then
-    updateSongTimeUsed(arg[3], arg[6])
-  end
 
-  if event == "CHAT_MESSAGE" then
-    if arg[5] == "!bard_hold_on" then
-      SaveSettings(true)
-    end
-
-    if arg[5] == "!bard_hold_off" then
-      SaveSettings(false)
-    end
-  end
-end
-
-
+-- START
 local function OnLoad()
-  settings = api.GetSettings("BardHelper")
+	settings = api.GetSettings("BardHelper")
 
-  api.Log:Info("Bard Helper Initialized")
+	api.Log:Info("Loaded " .. bard_helper.name .. " v" ..
+					 bard_helper.version .. " by " .. bard_helper.author)
+	api.On("UPDATE", OnUpdate)
 
-  for i = 1, #songsTimeRemains do
-    createSongUI(songsTimeRemains[i])
-  end
 
-  api.On("UPDATE", OnUpdate)
-  Canvas:SetHandler("OnEvent", Canvas.OnEvent)
-  Canvas:RegisterEvent("COMBAT_MSG")
-  Canvas:RegisterEvent("CHAT_MESSAGE")
+	
+	CreateMainDisplay(settings)
+
+	api.On("UPDATE", OnUpdate)
+	Canvas:SetHandler("OnEvent", Canvas.OnEvent)
+	Canvas:RegisterEvent("COMBAT_MSG")
+	Canvas:RegisterEvent("CHAT_MESSAGE")
+    --Canvas:SetHandler("OnDragStart", Canvas.OnDragStart)
+    --Canvas:SetHandler("OnDragStop", Canvas.OnDragStop)
 end
 
 
